fb01d61728871255205a876c96e3ed3e0005b618
patchgi <pacchigi0910@gmail.com>
2016-03-17 15:05:50 +0900

    fix code

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ def zundoko():
 
     while result!= 0b011110:
         rand=random.randint(0,1)
-        print ((u"ドコ",u"ズン")[rand])
+        print (u"ドコ",u"ズン")[rand]
         result=(result<<1)+rand&0b111111
 
-    print u"キ・ヨ・シ"
+    print u"キ・ヨ・シ!"
 if __name__=="__main__":
     zundoko()
