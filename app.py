843c93fb410ed76e8c4ef00370b88f36d8fb4447
patchgi <pacchigi0910@gmail.com>
2016-03-17 14:59:42 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+import random
+
+def zundoko():
+    result=0
+
+    while result!= 0b011110:
+        rand=random.randint(0,1)
+        print ((u"ドコ",u"ズン")[rand])
+        result=(result<<1)+rand&0b111111
+
+    print u"キ・ヨ・シ"
+if __name__=="__main__":
+    zundoko()
